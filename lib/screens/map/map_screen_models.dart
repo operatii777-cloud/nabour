151a38ddf3b3489ecab01a047702b56d11c27980
operatii777-cloud <operatii.777@gmail.com>
2026-04-07 18:27:02 +0300

    Add MapNeighborInfo and MapMagicEvent modelsAdd MapNeighborInfo and MapMagicEvent models

---
+++ lib/screens/map/map_screen_models.dart
@@ -0,0 +1,14 @@
+class MapNeighborInfo {
+    final String id;
+    final String name;
+    final String avatar;
+    MapNeighborInfo({required this.id, required this.name, required this.avatar});
+}
+
+class MapMagicEvent {
+    final String id;
+    final String title;
+    final double lat;
+    final double lng;
+    MapMagicEvent({required this.id, required this.title, required this.lat, required this.lng});
+}
