09d15c4d3ffc3ae29cfe1c58c258697a4eceef1e
operatii777-cloud <operatii.777@gmail.com>
2026-04-07 18:24:41 +0300

    Add MapScreenConstants class with constant valuesAdd map_screen_constants.dart

---
+++ lib/screens/map/map_screen_constants.dart
@@ -0,0 +1,8 @@
+class MapScreenConstants {
+    static const String driverLayerId = 'driver-layer';
+    static const String passengerLayerId = 'passenger-layer';
+    static const String neighborSourceId = 'neighbors-source';
+    static const double defaultIconSize = 0.5;
+    static const double selectedIconSize = 0.8;
+    static const int mapDebounceMs = 300;
+}
